ff89e51c612c772ca3054ca8301fa662ed43d3e2
krampcg23 <krampcg23@gmail.com>
2019-04-18 15:05:32 -0600

    Making small edits

--- prefetch.cpp
+++ prefetch.cpp
@@ -1,6 +1,7 @@
 #include "prefetch.h"
 #include <iostream>
 #include "system.h"
+#define SHIFTER 64 // this is the same as 2 ^ sys->SET_SHIFT
 
 int NullPrefetch::prefetchMiss(uint64_t address __attribute__((unused)),
                               unsigned int tid __attribute__((unused)),
@@ -17,9 +18,7 @@ int NullPrefetch::prefetchHit(uint64_t address __attribute__((unused)),
 
 int AdjPrefetch::prefetchMiss(uint64_t address, unsigned int tid, System* sys)
 {
-   uint64_t shifter = 1 << sys->SET_SHIFT;
-   address = address + shifter;
-   sys->memAccess(address, 'R', tid, true);
+   sys->memAccess(address + SHIFTER, 'R', tid, true);
    return 1;
 }
 
@@ -32,17 +31,16 @@ int AdjPrefetch::prefetchHit(uint64_t address, unsigned int tid, System* sys)
 int SeqPrefetch::prefetchMiss(uint64_t address, unsigned int tid, System* sys)
 {
    // Prefetch sequentially only if the previous miss was the one right before
-   uint64_t shifter = 1 << sys->SET_SHIFT;
    uint64_t mySet = address & sys->SET_MASK;
    uint64_t myTag = address & sys->TAG_MASK;
 
-   if (myTag == MRM_tag && mySet == (MRM_set + shifter)){
+   if (myTag == MRM_tag && mySet == (MRM_set + SHIFTER)){
        for(int i = 1; i <= n; i++) {
-          sys->memAccess(address + (shifter * i), 'R', tid, true);
+          sys->memAccess(address + (SHIFTER * i), 'R', tid, true);
        }
-       MRPrefetch = address + shifter;
-       MRP_set = (address + shifter) & sys->SET_MASK;
-       MRP_tag = (address + shifter)  & sys->TAG_MASK;
+       MRPrefetch = address + SHIFTER;
+       MRP_set = (address + SHIFTER) & sys->SET_MASK;
+       MRP_tag = (address + SHIFTER)  & sys->TAG_MASK;
    }
    MRM_set = address & sys->SET_MASK;
    MRM_tag = address & sys->TAG_MASK;
@@ -57,9 +55,8 @@ int SeqPrefetch::prefetchHit(uint64_t address, unsigned int tid, System* sys)
    uint64_t myTag = address & sys->TAG_MASK;
 
    if (myTag == MRP_tag && mySet == MRP_set) {
-        uint64_t shifter = 1 << sys->SET_SHIFT;
-        sys->memAccess(address + (shifter * n), 'R', tid, true);
-        MRPrefetch += shifter;
+        sys->memAccess(address + (SHIFTER * n), 'R', tid, true);
+        MRPrefetch += SHIFTER;
         MRP_set = MRPrefetch & sys->SET_MASK;
         MRP_tag = MRPrefetch & sys->TAG_MASK;
    }
@@ -68,16 +65,14 @@ int SeqPrefetch::prefetchHit(uint64_t address, unsigned int tid, System* sys)
 
 int BestEffortPrefetch::prefetchMiss(uint64_t address, unsigned int tid, System* sys)
 {
-   uint64_t shifter = 1 << sys->SET_SHIFT;
-   for (int i = 1; i <= 7; i++) {
-       sys->memAccess(address + (shifter * i), 'R', tid, true);
+   for (int i = 1; i <= 3; i++) {
+       sys->memAccess(address + (SHIFTER * i), 'R', tid, true);
    }
-   return 7;
+   return 3;
 }
 
 int BestEffortPrefetch::prefetchHit(uint64_t address, unsigned int tid, System* sys)
 {
-   uint64_t shifter = 1 << sys->SET_SHIFT;
-   sys->memAccess(address + shifter, 'R', tid, true);
+   sys->memAccess(address + SHIFTER, 'R', tid, true);
    return 1;
 }
